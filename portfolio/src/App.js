a850be4f5f3816b0e16f7d15ad0adff6eaa7caa6
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-07-19 20:52:30 +0200

    nav + router dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Nav from "./nav/Nav.js";
+import About from "./about/About.js";
+import Skills from "./skills/Skills.js";
+import Projects from "./projects/Projects.js";
+import Contact from "./contact/Contact.js";
+
 const App = () => {
-  return <>
-  <Nav/>
-  </>;
-  
-}
+  return (
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={About}/>
+        <Route path="/skills" element={Skills}/>
+        <Route path="/projects" element={Projects}/>
+        <Route path="/contact" element={Contact}/>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
