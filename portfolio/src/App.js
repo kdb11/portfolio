198de854e2bc366a8d9a57d8e612ef9c8de664d6
kdb11 <112870790+kdb11@users.noreply.github.com>
2023-07-20 14:05:08 +0200

    nav and styles

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import About from "./about/About.js";
 import Skills from "./skills/Skills.js";
 import Projects from "./projects/Projects.js";
 import Contact from "./contact/Contact.js";
+import "./styles/app.css";
 
 const App = () => {
   return (
